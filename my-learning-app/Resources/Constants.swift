1de925d6d9b48e0511a281e454d79301c84d99ca
Steve Hong <ponoponong@gmail.com>
2022-03-08 13:41:32 +0900

    - Make Constants.swift to change video string in data.json to url string - Make currentLesson and index in ContentModel.swift - Make beginLesson, hasNextLesson, nextLesson functions in ContentModel.swift to show current lesson and move to the next lesson - Add navigationViewStyle(.stack) in HomeView (top of the navigationView)

---
+++ my-learning-app/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  my-learning-app
+//
+//  Created by 홍성범 on 2022/03/08.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
--- my-learning-app/ViewModels/ContentModel.swift
+++ my-learning-app/ViewModels/ContentModel.swift
@@ -16,6 +16,10 @@ class ContentModel: ObservableObject {
     @Published var currentModule: Module?
     var currentModuleIndex = 0
     
+    // Current lesson
+    @Published var currentLesson: Lesson?
+    var currentLessonIndex = 0
+    
     var styleData: Data?
     
     init() {
@@ -74,5 +78,39 @@ class ContentModel: ObservableObject {
         
         // Set the current module
         currentModule = modules[currentModuleIndex]
+    }
+    
+    func beginLesson(_ lessonIndex: Int) {
+        
+        // Check that the lesson index is within range of module lessons
+        if lessonIndex < currentModule!.content.lessons.count {
+            currentLessonIndex = lessonIndex
+        } else {
+            currentLessonIndex = 0
+        }
+        
+        // Set the current lesson
+        currentLesson = currentModule!.content.lessons[currentLessonIndex]
+    }
+    
+    func hasNextLesson() -> Bool {
+        
+        return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
+    }
+    
+    func nextLesson() {
+        
+        // Advance the lesson
+        currentLessonIndex += 1
+        
+        // Check that it is within range
+        if currentLessonIndex < currentModule!.content.lessons.count {
+            // Set the current lesson property
+            currentLesson = currentModule!.content.lessons[currentLessonIndex]
+        } else {
+            // Reset the lesson state
+            currentLesson = nil
+            currentLessonIndex = 0
+        }
     }
 }
